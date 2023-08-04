ec8b547046ae1441b3b1ae13b075631517726034
Newton Duarte <newton_@hotmail.com>
2023-08-04 20:53:07 -0300

    feat: add get static props to home page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,25 @@
 import SectionHero from '@/components/SectionHero';
+import client from '@/graphql/client';
+import { GET_LANDING_PAGE } from '@/graphql/queries/getLandingPage';
+import { LandingPageProps, LandingPageResponse } from '@/types/api';
+import { GetStaticProps } from 'next';
 
-export default function Home() {
+export default function Home({ logo }: LandingPageProps) {
   return (
     <>
-      <SectionHero />
+      <SectionHero logo={logo} />
     </>
   );
 }
+
+export const getStaticProps: GetStaticProps = async () => {
+  const { landingPage } = await client.request<LandingPageResponse>(
+    GET_LANDING_PAGE
+  );
+
+  return {
+    props: {
+      logo: landingPage.data.attributes.logo.data.attributes,
+    },
+  };
+};
