d836dc2cd15ebe58b924d2fcc022488a58e5b870
Newton Duarte <newton_@hotmail.com>
2023-08-05 18:34:11 -0300

    feat: add header and section about project data

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,13 +1,15 @@
+import { SectionAboutProject } from '@/components/SectionAboutProject';
 import SectionHero from '@/components/SectionHero';
 import client from '@/graphql/client';
 import { GET_LANDING_PAGE } from '@/graphql/queries/getLandingPage';
 import { LandingPageProps, LandingPageResponse } from '@/types/api';
 import { GetStaticProps } from 'next';
 
-export default function Home({ logo }: LandingPageProps) {
+export default function Home({ logo, header, about }: LandingPageProps) {
   return (
     <>
-      <SectionHero logo={logo} />
+      <SectionHero logo={logo} header={header} />
+      <SectionAboutProject about={about} />
     </>
   );
 }
@@ -20,6 +22,8 @@ export const getStaticProps: GetStaticProps = async () => {
   return {
     props: {
       logo: landingPage.data.attributes.logo.data.attributes,
+      header: landingPage.data.attributes.header,
+      about: landingPage.data.attributes.sectionAboutProject,
     },
   };
 };
