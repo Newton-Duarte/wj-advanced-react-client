4465df2afcaed4fd1767895b10c1f784f069101c
Newton Duarte <newton_@hotmail.com>
2023-08-05 18:33:55 -0300

    feat: add landing pages graphql fragments and new queries

--- src/graphql/queries/getLandingPage.ts
+++ src/graphql/queries/getLandingPage.ts
@@ -1,30 +1,56 @@
-// const logo = /* GraphQL */ `
-//   fragment logo on LandingPage {
-//     logo {
-//       data {
-//         attributes {
-//           ...imageData
-//         }
-//       }
-//     }
-//   }
-// `;
-
 export const GET_LANDING_PAGE = /* GraphQL */ `
+  fragment logo on LandingPage {
+    logo {
+      data {
+        attributes {
+          alternativeText
+          url
+        }
+      }
+    }
+  }
+
+  fragment header on LandingPage {
+    header {
+      title
+      description
+      image {
+        data {
+          attributes {
+            alternativeText
+            url
+          }
+        }
+      }
+      button {
+        label
+        url
+      }
+    }
+  }
+
+  fragment sectionAboutProject on LandingPage {
+    sectionAboutProject {
+      title
+      description
+      image {
+        data {
+          attributes {
+            alternativeText
+            url
+          }
+        }
+      }
+    }
+  }
+
   query GET_LANDING_PAGE {
     landingPage {
       data {
-        id
         attributes {
-          logo {
-            data {
-              attributes {
-                alternativeText
-                url
-              }
-            }
-          }
-          createdAt
+          ...logo
+          ...header
+          ...sectionAboutProject
         }
       }
     }
