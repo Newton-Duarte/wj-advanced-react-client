c9a588a7c1442b876ae7ca2ab72e7bd1056ef7ff
Newton Duarte <newton_@hotmail.com>
2023-08-05 18:33:02 -0300

    feat: add heading component

---
+++ src/components/Heading/index.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+
+import * as S from './styles';
+
+export type Props = {
+  reverseColor?: boolean;
+  lineBottom?: boolean;
+  children: string;
+};
+
+export const Heading: React.FC<Props> = ({
+  reverseColor = false,
+  lineBottom = false,
+  children,
+}) => (
+  <S.Wrapper reverseColor={reverseColor} lineBottom={lineBottom}>
+    {children}
+  </S.Wrapper>
+);
