77bc594504aa2e3e7b5dc51410bd6360ae0c045a
Newton Duarte <newton_@hotmail.com>
2023-08-08 19:53:06 -0300

    feat: add card module component

---
+++ src/components/CardModule/index.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+
+import * as S from './styles';
+
+type Props = {
+  title: string;
+  subTitle: string;
+  children: React.ReactNode;
+};
+
+export const CardModule: React.FC<Props> = ({ title, subTitle, children }) => (
+  <S.Card>
+    <S.TitleWrapper>
+      <S.Title>{title}</S.Title>
+      <S.SubTitle>{subTitle}</S.SubTitle>
+    </S.TitleWrapper>
+
+    <S.Content>{children}</S.Content>
+  </S.Card>
+);
